7be6bf1d6466c92e3e05fae61cd7319bc3886a04
TerrenceChan <roxes2112@yahoo.com>
2018-02-17 01:12:49 +0800

    *Created an initial AI pathfinding with A* which failed.

---
+++ AI Pathfinding Assignment/Assets/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector2 worldPosition;
+    public int gridX;
+    public int gridY;
+
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
+    {
+        _walkable = walkable;
+        _worldPos = worldPosition;
+        _gridX = gridX;
+        _gridY = gridY;
+    }
+}
